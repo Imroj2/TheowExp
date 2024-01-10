1bd6c9d2d519b4965c8bade0802673738e6f004c
Dell <Dell@DESKTOP-FM8464J>
2024-01-10 07:45:48 +0545

    Add project files.

---
+++ TheowExp/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            //code that might throw an exception
+            int age = 5;
+            if(age<0)
+            {
+                throw new ArgumentException("Age cannot be negative");
+            }
+        }
+        catch(ArgumentException ex)
+        {
+           //Handle the exception
+           Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
